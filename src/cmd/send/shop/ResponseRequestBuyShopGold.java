9aa4b900cae95cb8866685ec84a81510d4ac6840
CPU60274\CPU60274-local <sondxm@vng.com.vn>
2022-06-20 13:11:10 +0700

    Shop Gold

---
+++ src/cmd/send/shop/ResponseRequestBuyShopGold.java
@@ -0,0 +1,29 @@
+package cmd.send.shop;
+import bitzero.server.extensions.data.BaseMsg;
+import cmd.CmdDefine;
+import model.Shop.ShopDTO;
+
+import java.nio.ByteBuffer;
+
+public class ResponseRequestBuyShopGold extends BaseMsg {
+    public ShopDTO shopDTO;
+    public short error;
+    public ResponseRequestBuyShopGold(short _error, ShopDTO shopDTO) {
+        super(CmdDefine.BUY_GOLD_SHOP);
+        this.shopDTO=shopDTO;
+        error= _error;
+    }
+
+    @Override
+    public byte[] createData() {
+        ByteBuffer bf = makeBuffer();
+        bf.putInt(shopDTO.getGoldChange());
+        bf.putInt(shopDTO.getGemChange());
+        //System.out.println("response buy shop gold");
+//        for(int i=0;i<shopDTO.itemList.size();i++){
+//            bf.putInt(shopDTO.itemList.get(i).getItemType());
+//            bf.putInt(shopDTO.itemList.get(i).getQuantity());
+//        }
+        return packBuffer(bf);
+    }
+}
