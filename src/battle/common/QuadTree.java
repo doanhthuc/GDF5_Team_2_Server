bf1f7103d135c290030ee09eb821716a4f788684
CPU60274\CPU60274-local <sondxm@vng.com.vn>
2022-07-22 14:44:11 +0700

    delete Line 115 Quadtree

--- src/battle/common/QuadTree.java
+++ src/battle/common/QuadTree.java
@@ -112,7 +112,6 @@ public class QuadTree {
             }
         }
         returnObjects.addAll(this.listObjects);
-        System.out.println("QuadTree line 115 " + returnObjects.size());
         return returnObjects;
     }
 
