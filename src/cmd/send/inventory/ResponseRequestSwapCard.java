9e000b3b0a032bf7ffc8dc55af066e26cf1e2b01
CPU60274\CPU60274-local <sondxm@vng.com.vn>
2022-08-02 09:58:50 +0700

    Add swap card API

--- src/cmd/send/inventory/ResponseRequestSwapCard.java
+++ src/cmd/send/inventory/ResponseRequestSwapCard.java
@@ -1,2 +1,30 @@
-package cmd.send.inventory;public class ResponseRequestSwapCard {
+package cmd.send.inventory;
+
+import bitzero.server.extensions.data.BaseMsg;
+import cmd.CmdDefine;
+import model.Inventory.InventoryDTO;
+
+import java.nio.ByteBuffer;
+
+public class ResponseRequestSwapCard extends BaseMsg {
+    public int cardInID ;
+    public int cardOutID ;
+    public short error;
+
+    public ResponseRequestSwapCard(short _error) {
+        super(CmdDefine.UPGRADE_CARD,_error);
+    }
+    public ResponseRequestSwapCard(short _error, int cardInID, int cardOutID) {
+        super(CmdDefine.UPGRADE_CARD,_error);
+        this.cardInID = cardInID;
+        this.cardOutID = cardOutID;
+    }
+
+    @Override
+    public byte[] createData() {
+        ByteBuffer bf = makeBuffer();
+        bf.putInt(this.cardInID);
+        bf.putInt(this.cardOutID);
+        return packBuffer(bf);
+    }
 }
