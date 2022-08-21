c1ae225145c0b2c60746792493c1e05a8ecf0b1c
vyhv <hovanvydut@gmail.com>
2022-08-21 17:23:46 +0700

    snapshot spell

--- src/battle/factory/EntityFactory.java
+++ src/battle/factory/EntityFactory.java
@@ -783,7 +783,7 @@ public class EntityFactory {
         int typeID = GameConfig.ENTITY_ID.FIRE_SPELL;
         EntityECS entity = this._createEntity(typeID, mode);
 
-        double S = 3000;
+        double S = GameConfig.BATTLE.DELAY_SPELL * 1000;
         double V = 1000;
         double T = S / V;
 
